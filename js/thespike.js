990a10cb4e108c93d449aed066d7821265db7664
Boyrak <90590078+Boyrak@users.noreply.github.com>
2023-08-06 10:22:41 +0700

    thespike.js

---
+++ js/thespike.js
@@ -0,0 +1,15 @@
+function Rak2() {
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391594', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('30000', 'I32'); 
+    
+}
+  
+function Rak4() {
+     
+   h5gg.clearResults();
+                    h5gg.searchNumber('-2079391714', 'I32', '0x100000000', '0x200000000');
+  h5gg.searchNearby('100302', 'I32', '0x100');
+            h5gg.searchNumber('-2079391714', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('-30000', 'I32' ); 
+}

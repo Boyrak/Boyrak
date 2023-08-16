78c34a87e94b150b16dc8a7731a61e4299655102
Boyrak <90590078+Boyrak@users.noreply.github.com>
2023-08-16 09:03:39 +0700

    carparkingRXK.js

--- js/ff.js
+++ js/ff.js
@@ -1,1 +1,150 @@
 
+
+function link2(){
+   location.href="https://t.me/IPA_FREE_RAK";
+}
+
+function link3(){
+   location.href="https://t.me/rakmodvip";
+}
+
+function link4(){
+   location.href="https://t.me/RcSrAK";
+  
+}
+
+   function hp() {
+      console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('150', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('1500', 'F32'); 
+    document.getElementById("myButton3").removeEventListener("click", hp);
+}
+ document.getElementById("myButton3").addEventListener("click", hp);
+
+  function tor() {
+     console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('220', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('2000', 'F32'); 
+    document.getElementById("myButton3").removeEventListener("click", tor);
+}
+ document.getElementById("myButton3").addEventListener("click", tor);
+
+  
+  function hp2() {
+     console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('5900', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('1111', 'F32'); 
+    document.getElementById("myButton3").removeEventListener("click", hp2);
+}
+ document.getElementById("myButton3").addEventListener("click", hp2);
+
+  
+   function tor2() {
+      console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('4100', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('1300', 'F32'); 
+    document.getElementById("myButton3").removeEventListener("click", tor2);
+}
+ document.getElementById("myButton3").addEventListener("click", tor2);
+
+  
+  
+   
+   function gr() {
+      console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('6', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('1000', 'F32'); 
+    document.getElementById("myButton3").removeEventListener("click", gr);
+
+}
+ document.getElementById("myButton3").addEventListener("click", gr);
+
+    function s() {
+       console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('0.1', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('9.999999E-32', 'F32'); 
+    document.getElementById("myButton3").removeEventListener("click", s);
+
+}
+  document.getElementById("myButton3").addEventListener("click", s);
+    function s2() {
+       console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('0.2', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('9.999999E-32', 'F32'); 
+     document.getElementById("myButton3").removeEventListener("click", s2);
+}
+    document.getElementById("myButton3").addEventListener("click", s2);
+  function s3() {
+     console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('0.3', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('9.999999E-32', 'F32'); 
+      document.getElementById("myButton3").removeEventListener("click", s3);
+    
+}
+  document.getElementById("myButton3").addEventListener("click", s3);
+
+  function s4() {
+     console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('0.4', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('9.999999E-32', 'F32'); 
+      document.getElementById("myButton3").removeEventListener("click", s4);
+    
+}
+document.getElementById("myButton3").addEventListener("click", s4);
+  function s5() {
+    console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('0.5', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('9.999999E-32', 'F32'); 
+    document.getElementById("myButton3").removeEventListener("click", s5);
+    
+}
+document.getElementById("myButton3").addEventListener("click", s5);
+    function gl() {
+    console.log("Button clicked!");
+    h5gg.clearResults();
+    h5gg.searchNumber('150', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('1695', 'F32'); 
+    h5gg.clearResults();
+    h5gg.searchNumber('220', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('2000', 'F32'); 
+      h5gg.clearResults();
+    h5gg.searchNumber('5900', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('1111', 'F32'); 
+      h5gg.clearResults();
+    h5gg.searchNumber('4100', 'F32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('1300', 'F32');  
+    document.getElementById("myButton3").removeEventListener("click", gl);
+}
+document.getElementById("myButton3").addEventListener("click", gl);
+function glll() 
+{
+
+  
+       if (remember.checked == 1)
+    {
+       h5gg.clearResults();
+      h5gg.searchNumber('700', 'F32', '0x0', '0x200000000');
+      h5gg.editAll('500168', 'F32');
+      h5gg.clearResults();
+    }
+    else 
+    {
+            h5gg.clearResults();
+      h5gg.searchNumber('500168', 'F32', '0x0', '0x200000000');
+      h5gg.editAll('700', 'F32');
+      h5gg.clearResults();
+    }
+ 
+
+
+}

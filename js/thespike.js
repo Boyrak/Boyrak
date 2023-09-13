058a703f0d89c70b404d59646c1f2a3f449a85cb
Boyrak <90590078+Boyrak@users.noreply.github.com>
2023-09-13 18:32:12 +0700

    thespike.js

--- js/thespike.js
+++ js/thespike.js
@@ -13,3 +13,58 @@ function Rak4() {
             h5gg.searchNumber('-2079391714', 'I32', '0x100000000', '0x200000000');
   h5gg.editAll('-30000', 'I32' ); 
 }
+
+function 7() {
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391737', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('1000', 'I32'); 
+    
+}
+ 
+
+function 8() {
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391736', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('1000', 'I32'); 
+    
+}
+        
+        function 12() {
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391732', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('1000', 'I32'); 
+    
+}
+    function 13() {
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391731', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('1000', 'I32'); 
+    
+}    
+        function 15() {
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391729', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('1000', 'I32'); 
+    
+}    
+        function 20() {
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391724', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('1000', 'I32'); 
+    
+}    
+        
+        function 202() {
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391724', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('5000', 'I32'); 
+    
+}    
+        
+        function win() {
+    h5gg.clearResults();
+    h5gg.searchNumber('1', 'F64', '0x100000000', '0x200000000'); 
+          h5gg.searchNumber('2', 'F64', '0x100000000', '0x200000000'); 
+      h5gg.editAll('1000', 'F64'); 
+    
+}

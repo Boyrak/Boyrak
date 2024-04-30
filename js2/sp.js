8e7793a8a0a3e1ce2d398c3cedd5db7c8a0e9037
Boyrak <90590078+Boyrak@users.noreply.github.com>
2024-04-30 20:29:40 +0700

    Add files via upload

---
+++ js2/sp.js
@@ -0,0 +1,142 @@
+  
+    function Rakwin2() {
+        h5gg.clearResults();
+        h5gg.searchNumber('1', 'F64', '0x100000000', '0x200000000'); } 
+           
+            function Rakwin3() {
+        h5gg.searchNumber('2', 'F64', '0x100000000', '0x200000000'); 
+          h5gg.editAll('1000', 'F64'); 
+              } 
+          function Rakwin4() {
+        h5gg.searchNumber('1', 'F64', '0x100000000', '0x200000000'); 
+          h5gg.editAll('1000', 'F64'); } 
+    
+    
+    function Rak150() {
+         
+       h5gg.clearResults();
+                        h5gg.searchNumber('-2079391594', 'I32', '0x100000000', '0x200000000');
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391594', 'I32', '0x100000000', '0x200000000');
+      h5gg.editAll('30000', 'I32' ); 
+    }
+    
+    function Rak7() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391737', 'I32', '0x100000000', '0x200000000'); 
+         
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391737', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('30000', 'I32' ); 
+        
+    }
+     function Rak10() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391734', 'I32', '0x100000000', '0x200000000'); 
+         
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391734', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('30000', 'I32' ); 
+        
+    }
+            function Rak5() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391739', 'I32', '0x100000000', '0x200000000'); 
+         
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391739', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('30000', 'I32' ); 
+        
+    }
+    
+    function Rak8() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391736', 'I32', '0x100000000', '0x200000000'); 
+       
+          
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391736', 'I32', '0x100000000', '0x200000000');
+      h5gg.editAll('30000', 'I32' ); 
+        
+    }
+            
+            function Rak12() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391732', 'I32', '0x100000000', '0x200000000'); 
+          
+          
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391732', 'I32', '0x100000000', '0x200000000');
+      h5gg.editAll('30000', 'I32' ); 
+        
+    }
+        function Rak13() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391731', 'I32', '0x100000000', '0x200000000'); 
+      
+          
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391731', 'I32', '0x100000000', '0x200000000');
+      h5gg.editAll('30000', 'I32' ); 
+        
+    }    
+            function Rak15() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391729', 'I32', '0x100000000', '0x200000000'); 
+          
+          
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391729', 'I32', '0x100000000', '0x200000000');
+      h5gg.editAll('30000', 'I32' ); 
+        
+    }    
+            function Rak20() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391724', 'I32', '0x100000000', '0x200000000'); 
+       
+          
+      h5gg.searchNearby('227', 'I32', '0x100');
+                h5gg.searchNumber('-2079391724', 'I32', '0x100000000', '0x200000000');
+      h5gg.editAll('30000', 'I32' ); 
+        
+    }    
+            
+            
+            
+            function Rakwin() {
+        h5gg.clearResults();
+        h5gg.searchNumber('1', 'F64', '0x100000000', '0x200000000'); 
+              h5gg.searchNumber('2', 'F64', '0x100000000', '0x200000000'); 
+          h5gg.editAll('1000', 'F64'); 
+        
+    }
+            
+            function blue() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391621', 'I32', '0x100000000', '0x200000000'); 
+              h5gg.searchNearby('-2079391423', 'I32', '0x100');
+              h5gg.searchNumber('-2079391621', 'I32', '0x100000000', '0x200000000'); 
+              h5gg.searchNearby('226', 'I32', '0x100');
+              h5gg.searchNearby('256', 'I32', '0x100');
+              h5gg.searchNumber('-2079391621', 'I32', '0x100000000', '0x200000000'); 
+          h5gg.editAll('-2079361581', 'I32'); 
+        
+    }
+            function s() {
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391645', 'I32', '0x100000000', '0x200000000'); 
+          h5gg.editAll('2', 'I32'); 
+        
+    }      function ModNishi(){
+    
+        h5gg.clearResults();
+        h5gg.searchNumber('-2079391718', 'I32', '0x100000000', '0x200000000'); 
+        h5gg.searchNearby('-2079391727', 'I32', '0x100');
+        h5gg.searchNearby('-2079391726', 'I32', '0x100');
+        h5gg.searchNearby('-2079391564', 'I32', '0x100');
+        h5gg.searchNumber('-2079391564', 'I32', '0x100000000', '0x200000000'); 
+        h5gg.editAll('-2079391594', 'I32'); 
+    }
+            
+    
+    

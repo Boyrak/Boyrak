b96dbf46e099f0c279e8499cfa7a1bf98c2a7ce8
Boyrak <90590078+Boyrak@users.noreply.github.com>
2024-05-01 21:40:10 +0700

    Add files via upload

---
+++ js2/sp2.js
@@ -0,0 +1,425 @@
+var checkboxesClicked = {};
+
+function toggleCheckboxAndCall(checkbox, functionName) {
+    if (!checkboxesClicked[functionName]) {
+        window[functionName]();
+        checkboxesClicked[functionName] = true;
+    }
+    checkbox.checked = !checkbox.checked;
+}
+
+function blue() {
+    console.log("blue function called");
+    // Add your Rak12 functionality here
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391621', 'I32', '0x100000000', '0x200000000'); 
+          h5gg.searchNearby('-2079391423', 'I32', '0x100');
+          h5gg.searchNumber('-2079391621', 'I32', '0x100000000', '0x200000000'); 
+          h5gg.searchNearby('226', 'I32', '0x100');
+          h5gg.searchNearby('256', 'I32', '0x100');
+          h5gg.searchNumber('-2079391621', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('-2079361581', 'I32'); 
+                              
+var message = "Ready.....";
+
+var alertBox = document.createElement('div');
+alertBox.className = 'alert';
+
+// Create the close button
+var closeButton = document.createElement('span');
+closeButton.className = 'closebtn';
+closeButton.innerHTML = '&times;';
+closeButton.onclick = function() {
+alertBox.style.display = 'none';
+};
+
+// Create the message
+var messageNode = document.createElement('strong');
+messageNode.innerHTML = message + ' ';
+
+// Create the Telegram button
+
+
+
+
+// Append elements to the alert box
+alertBox.appendChild(closeButton);
+alertBox.appendChild(messageNode);
+
+
+// Append the alert box to the body
+document.body.appendChild(alertBox);
+
+// Center the alert box
+alertBox.style.position = 'fixed';
+alertBox.style.top = '50%';
+alertBox.style.left = '50%';
+alertBox.style.transform = 'translate(-50%, -50%)';
+}
+
+
+
+
+
+
+
+
+function ball57810() {
+    console.log("ball57810 function called");
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391739', 'I32', '0x100000000', '0x200000000'); 
+     
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391739', 'I32', '0x100000000', '0x200000000'); 
+  h5gg.editAll('30000', 'I32' ); 
+    
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391737', 'I32', '0x100000000', '0x200000000'); 
+     
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391737', 'I32', '0x100000000', '0x200000000'); 
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391736', 'I32', '0x100000000', '0x200000000'); 
+   
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391736', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391734', 'I32', '0x100000000', '0x200000000'); 
+     
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391734', 'I32', '0x100000000', '0x200000000'); 
+  h5gg.editAll('30000', 'I32' ); 
+                              
+var message = "Ready.....";
+
+var alertBox = document.createElement('div');
+alertBox.className = 'alert';
+
+// Create the close button
+var closeButton = document.createElement('span');
+closeButton.className = 'closebtn';
+closeButton.innerHTML = '&times;';
+closeButton.onclick = function() {
+alertBox.style.display = 'none';
+};
+
+// Create the message
+var messageNode = document.createElement('strong');
+messageNode.innerHTML = message + ' ';
+
+// Create the Telegram button
+
+
+
+
+// Append elements to the alert box
+alertBox.appendChild(closeButton);
+alertBox.appendChild(messageNode);
+
+
+// Append the alert box to the body
+document.body.appendChild(alertBox);
+
+// Center the alert box
+alertBox.style.position = 'fixed';
+alertBox.style.top = '50%';
+alertBox.style.left = '50%';
+alertBox.style.transform = 'translate(-50%, -50%)';
+}
+
+
+
+
+
+
+
+
+
+
+function ball12() {
+    console.log("ball12 function called");
+    // Add your ball2 functionality here
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391732', 'I32', '0x100000000', '0x200000000'); 
+      
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391732', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391731', 'I32', '0x100000000', '0x200000000'); 
+  
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391731', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391729', 'I32', '0x100000000', '0x200000000'); 
+      
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391729', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+    
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391724', 'I32', '0x100000000', '0x200000000'); 
+   
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391724', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+                    h5gg.searchNumber('-2079391594', 'I32', '0x100000000', '0x200000000');
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391594', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+
+                            
+var message = "Ready.....";
+
+var alertBox = document.createElement('div');
+alertBox.className = 'alert';
+
+// Create the close button
+var closeButton = document.createElement('span');
+closeButton.className = 'closebtn';
+closeButton.innerHTML = '&times;';
+closeButton.onclick = function() {
+alertBox.style.display = 'none';
+};
+
+// Create the message
+var messageNode = document.createElement('strong');
+messageNode.innerHTML = message + ' ';
+
+// Create the Telegram button
+
+
+
+
+// Append elements to the alert box
+alertBox.appendChild(closeButton);
+alertBox.appendChild(messageNode);
+
+
+// Append the alert box to the body
+document.body.appendChild(alertBox);
+
+// Center the alert box
+alertBox.style.position = 'fixed';
+alertBox.style.top = '50%';
+alertBox.style.left = '50%';
+alertBox.style.transform = 'translate(-50%, -50%)';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function ballall() {
+    console.log("ballall function called");
+    
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391739', 'I32', '0x100000000', '0x200000000'); 
+     
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391739', 'I32', '0x100000000', '0x200000000'); 
+  h5gg.editAll('30000', 'I32' ); 
+    
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391737', 'I32', '0x100000000', '0x200000000'); 
+     
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391737', 'I32', '0x100000000', '0x200000000'); 
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391736', 'I32', '0x100000000', '0x200000000'); 
+   
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391736', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391734', 'I32', '0x100000000', '0x200000000'); 
+     
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391734', 'I32', '0x100000000', '0x200000000'); 
+  h5gg.editAll('30000', 'I32' ); 
+    
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391732', 'I32', '0x100000000', '0x200000000'); 
+      
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391732', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391731', 'I32', '0x100000000', '0x200000000'); 
+  
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391731', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391729', 'I32', '0x100000000', '0x200000000'); 
+      
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391729', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+    
+  h5gg.clearResults();
+    h5gg.searchNumber('-2079391724', 'I32', '0x100000000', '0x200000000'); 
+   
+      
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391724', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+  h5gg.clearResults();
+                    h5gg.searchNumber('-2079391594', 'I32', '0x100000000', '0x200000000');
+  h5gg.searchNearby('227', 'I32', '0x100');
+            h5gg.searchNumber('-2079391594', 'I32', '0x100000000', '0x200000000');
+  h5gg.editAll('30000', 'I32' ); 
+                            
+var message = "Ready.....";
+
+var alertBox = document.createElement('div');
+alertBox.className = 'alert';
+
+// Create the close button
+var closeButton = document.createElement('span');
+closeButton.className = 'closebtn';
+closeButton.innerHTML = '&times;';
+closeButton.onclick = function() {
+alertBox.style.display = 'none';
+};
+
+// Create the message
+var messageNode = document.createElement('strong');
+messageNode.innerHTML = message + ' ';
+
+// Create the Telegram button
+
+
+
+
+// Append elements to the alert box
+alertBox.appendChild(closeButton);
+alertBox.appendChild(messageNode);
+
+
+// Append the alert box to the body
+document.body.appendChild(alertBox);
+
+// Center the alert box
+alertBox.style.position = 'fixed';
+alertBox.style.top = '50%';
+alertBox.style.left = '50%';
+alertBox.style.transform = 'translate(-50%, -50%)';
+  }
+
+
+  function rolls() {
+    console.log("rolls function called");
+    
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391645', 'I32', '0x100000000', '0x200000000'); 
+      h5gg.editAll('2', 'I32'); 
+                              
+var message = "Ready.....";
+
+var alertBox = document.createElement('div');
+alertBox.className = 'alert';
+
+// Create the close button
+var closeButton = document.createElement('span');
+closeButton.className = 'closebtn';
+closeButton.innerHTML = '&times;';
+closeButton.onclick = function() {
+alertBox.style.display = 'none';
+};
+
+// Create the message
+var messageNode = document.createElement('strong');
+messageNode.innerHTML = message + ' ';
+
+// Create the Telegram button
+
+
+
+
+// Append elements to the alert box
+alertBox.appendChild(closeButton);
+alertBox.appendChild(messageNode);
+
+
+// Append the alert box to the body
+document.body.appendChild(alertBox);
+
+// Center the alert box
+alertBox.style.position = 'fixed';
+alertBox.style.top = '50%';
+alertBox.style.left = '50%';
+alertBox.style.transform = 'translate(-50%, -50%)';
+    }
+
+
+
+
+    function ModNishi() {
+    console.log("ModNishi function called");
+    h5gg.clearResults();
+    h5gg.searchNumber('-2079391718', 'I32', '0x100000000', '0x200000000'); 
+    h5gg.searchNearby('-2079391727', 'I32', '0x100');
+    h5gg.searchNearby('-2079391726', 'I32', '0x100');
+    h5gg.searchNearby('-2079391564', 'I32', '0x100');
+    h5gg.searchNumber('-2079391564', 'I32', '0x100000000', '0x200000000'); 
+    h5gg.editAll('-2079391594', 'I32'); 
+                              
+var message = "Ready.....";
+
+var alertBox = document.createElement('div');
+alertBox.className = 'alert';
+
+// Create the close button
+var closeButton = document.createElement('span');
+closeButton.className = 'closebtn';
+closeButton.innerHTML = '&times;';
+closeButton.onclick = function() {
+alertBox.style.display = 'none';
+};
+
+// Create the message
+var messageNode = document.createElement('strong');
+messageNode.innerHTML = message + ' ';
+
+// Create the Telegram button
+
+
+
+
+// Append elements to the alert box
+alertBox.appendChild(closeButton);
+alertBox.appendChild(messageNode);
+
+
+// Append the alert box to the body
+document.body.appendChild(alertBox);
+
+// Center the alert box
+alertBox.style.position = 'fixed';
+alertBox.style.top = '50%';
+alertBox.style.left = '50%';
+alertBox.style.transform = 'translate(-50%, -50%)';
+    }
